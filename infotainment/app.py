2198ca06b13727d97eeedf28467956404aa2773d
Antti K <antti.kiiskinen@gmail.com>
2025-03-25 10:15:39 +0200

    Lunch option improvements (#5)
    * Added page refresh every 30 minutes
    
    * modified style
    
    * Added more lunch options
    
    * Title Change and Carousel perView increase

--- infotainment/app.py
+++ infotainment/app.py
@@ -8,7 +8,9 @@ LUNCH_URLS = [
     'https://lounaat.info/lounas/food-co-tulli-business-park/tampere',
     'https://lounaat.info/lounas/myllarit/tampere',
     'https://lounaat.info/lounas/old-mates-tampere/tampere',
-    'https://lounaat.info/lounas/aleksis/tampere'
+    'https://lounaat.info/lounas/aleksis/tampere',
+    'https://lounaat.info/lounas/edun-herkkukeidas-eetwartti/tampere',
+    'https://www.lounaat.info/lounas/le-momento-ratina/tampere'
 ]
 
 @app.route('/')
